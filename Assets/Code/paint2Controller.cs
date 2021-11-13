3a6ba56f442ddae1eb3276ef8434846a364c51fc
Itomasaki <ito-masaki728@g.ecc.u-tokyo.ac.jp>
2021-11-13 18:55:13 +0900

    add paintcube

--- Assets/Code/paint2Controller.cs
+++ Assets/Code/paint2Controller.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class paintcubeController : MonoBehaviour
+public class paint2Controller : MonoBehaviour
 {
     int color;
-    public static int yellowPoint;
-    public static int redPoint;
     void Start()
     {
       // 現在使用されているマテリアルを取得
@@ -22,10 +20,10 @@ public class paintcubeController : MonoBehaviour
         {
             Material mat = this.GetComponent<Renderer>().material;
             mat.color = new Color(1.0f,1.0f,0.0f,0.3f);
-            yellowPoint += 1;
+            paintcubeController.yellowPoint += 1;
             if (color == 2)
             {
-                redPoint -= 1;
+                paintcubeController.redPoint -= 1;
             }
             color  =  1 ; //黄色状態
         }
@@ -34,18 +32,19 @@ public class paintcubeController : MonoBehaviour
         {
             Material mat = this.GetComponent<Renderer>().material;
             mat.color = new Color(1.0f,0.0f,0.0f,0.3f);
-            redPoint += 1;
+            paintcubeController.redPoint += 1;
             if (color == 1)
             {
-                yellowPoint -=1;
+                paintcubeController.yellowPoint -=1;
             }
             color = 2 ;//赤色状態
         }
+        //Debug.Log(paintcubeController.yellowPoint);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(yellowPoint);
+        
     }
 }
