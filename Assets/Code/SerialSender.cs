1f5c965a71399adef7fe91470901c22ef1d38e92
DAICHI_INOUE <ground3101@gmail.com>
2021-11-14 17:19:19 +0900

    complete output system

--- Assets/Code/SerialSender.cs
+++ Assets/Code/SerialSender.cs
@@ -1,17 +1,17 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class SerialSender : MonoBehaviour
-// {
-//     //SerialHandler.cのクラス
-//     public SerialHandlerOut serialHandlerOut;
+public class SerialSender : MonoBehaviour
+{
+    //SerialHandler.cのクラス
+    public SerialHandlerOut serialHandlerOut;
 
-//     void Update() //ここは0.001秒ごとに実行される
-//     {
-//         if(Input.GetKeyDown(KeyCode.A))
-//         {
-//             serialHandlerOut.Write("1");
-//         }
-//     }
-// }
+    void Update() //ここは0.001秒ごとに実行される
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            serialHandlerOut.Write("1");
+        }
+    }
+}
